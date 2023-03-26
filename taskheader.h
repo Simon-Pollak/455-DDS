7482cdbcf88361623aa174b8ec989c6a27d9b93f
Simon-Pollak <32861789+Simon-Pollak@users.noreply.github.com>
2023-03-26 16:06:51 -0700

    Fixed static declarations

--- taskheader.h
+++ taskheader.h
@@ -64,37 +64,22 @@ typedef struct FromDDS_Message {
 	DD_Task_List tasks;
 }FromDDS_Message;
 
-/*
- * The queue send and receive tasks as described in the comments at the top of
- * this file.
- */
-//This function receives all of the information necessary to create a new dd_task struct (excluding
-//the release time and completion time). The struct is packaged as a message and sent to a queue
-//for the DDS to receive.
 void create_dd_task( TaskHandle_t t_handle,
 		enum task_type type,
 		uint32_t task_id,
 		uint32_t absolute_deadline);
 
-//This function receives the ID of the DD-Task which has completed its execution. The ID is packaged
-//as a message and sent to a queue for the DDS to receive.
 void delete_dd_task(uint32_t task_id);
 
-//This function sends a message to a queue requesting the Active Task List from the DDS. Once a
-//response is received from the DDS, the function returns the list.
 DD_Task_List* get_active_dd_task_list(void);
 
-//This function sends a message to a queue requesting the Completed Task List from the DDS. Once
-//a response is received from the DDS, the function returns the list.
 DD_Task_List* get_complete_dd_task_list(void);
 
-//This function sends a message to a queue requesting the Overdue Task List from the DDS. Once a
-//response is received from the DDS, the function returns the list.
 DD_Task_List* get_overdue_dd_task_list(void);
 
-static void DDS_Task( void *pvParameters );
-static void User_Defined1_Task( void *pvParameters );
-static void User_Defined2_Task( void *pvParameters );
-static void User_Defined3_Task( void *pvParameters );
-static void DDT_Generator_Task( void *pvParameters );
-static void Monitor_Task( void *pvParameters );
+void DDS_Task( void *pvParameters );
+void User_Defined1_Task( void *pvParameters );
+void User_Defined2_Task( void *pvParameters );
+void User_Defined3_Task( void *pvParameters );
+void DDT_Generator_Task( void *pvParameters );
+void Monitor_Task( void *pvParameters );
